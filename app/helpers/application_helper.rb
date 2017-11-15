627c313e6ff4f9766e1e2b18d6c17578339c8d67
Isashi <isashimitsui@gmail.com>
2017-11-15 10:08:40 +0100

    add a full title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	#return the full title based on the specific page
+	def full_title(page_title = "")
+		base_title = "3days travel"
+		if page_title.empty?
+			base_title
+		else
+			page_title + " | " + base_title
+		end
+	end
 end
