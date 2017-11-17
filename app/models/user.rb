b5e125cd985dc0eadc75a807716b145acb556c6b
Isashi <isashimitsui@gmail.com>
2017-11-17 10:35:27 +0100

    change login with username instead of email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, 
+  			 :registerable,
+  			 :recoverable, 
+  			 :rememberable, 
+  			 :trackable, 
+  			 :validatable, 
+  			 :authentication_keys => [:username]
+
+	validates :email, presence: true, length: {maximum: 255},
+									  uniqueness: {case_sensitive: false}
 end
