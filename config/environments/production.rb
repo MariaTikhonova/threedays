e6ee093b7e4cdf6c46a8c85f20716123a17fd8f5
Isashi <isashimitsui@gmail.com>
2017-11-23 11:10:10 +0100

    fix heroku host in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   #Settings for Sendgrid
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = 'three-days.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
