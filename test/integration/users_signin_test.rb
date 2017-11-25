af6241fb49f369489a8c9ecaf3ee866f0fd7727f
Isashi <isashimitsui@gmail.com>
2017-11-25 11:25:35 +0100

    add logout integration test

--- test/integration/users_signin_test.rb
+++ test/integration/users_signin_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class UsersSigninTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
   
   def setup
     @user = users(:ciaone)
@@ -28,5 +29,20 @@ class UsersSigninTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', new_user_session_path, count: 0
     assert_select 'a[href=?]', destroy_user_session_path
     assert_select 'a[href=?]', user_path(@user)
+  end
+
+  test 'sign in and logout' do
+    sign_in @user
+    get root_path
+    assert_select 'a[href=?]', new_user_session_path, count: 0
+    assert_select 'a[href=?]', destroy_user_session_path
+    delete destroy_user_session_path
+    assert_redirected_to root_path
+    follow_redirect!
+    assert_not flash.empty?
+    assert_template 'static_pages/home'
+    assert_select 'a[href=?]', new_user_session_path
+    assert_select 'a[href=?]', new_user_registration_path
+    assert_select 'a[href=?]', destroy_user_session_path, count: 0
   end
 end
