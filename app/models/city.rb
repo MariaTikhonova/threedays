81d682be6a33840a6b71fc2032f965e22bc72f86
MariaTikhonova <ritz.rkraft@gmail.com>
2017-11-28 14:48:08 +0200

    just changes at model

--- app/models/city.rb
+++ app/models/city.rb
@@ -3,7 +3,7 @@ class City < ApplicationRecord
 	has_many :places
 
 	has_attached_file :image, styles: { medium: "300x300", thumb: "100x100" }, default_url: "/images/paris.jpg"
-		#validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+		validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 	
 	
 end
